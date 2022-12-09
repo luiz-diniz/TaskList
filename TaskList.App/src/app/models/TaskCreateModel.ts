69f95480641abf2ce17841d7c4d04577a153fd9a
Luiz Diniz <luiz.gustavon@hotmail.com>
2022-12-09 19:22:04 -0300

    - task model and base services creation

---
+++ TaskList.App/src/app/models/TaskCreateModel.ts
@@ -0,0 +1,9 @@
+export class TaskCreateModel{
+    public Id: number;
+    public IdUser: number;
+    public Name: string;
+    public Description: string;
+    public Done: boolean;
+    public CreationDate: any;
+    public FinishDate: any;
+}
